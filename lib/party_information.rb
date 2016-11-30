bc5c8b80018f1df75ae769a9cd8c12a9a8f13923
Chris Mytton <chrismytton@gmail.com>
2016-11-30 14:18:17 +0100

    Extract PartyInformation class
    This class handles mapping a party name to a name + id combo.

---
+++ lib/party_information.rb
@@ -0,0 +1,66 @@
+class PartyInformation
+  MAPPING = {
+    AKP: ['Adalet ve Kalkınma Partisi'],
+    ANAP: ['Anavatan Partisi'],
+    AP:  ['Adalet Partisi'],
+    BBP: ['Büyük Birlik Partisi'],
+    BDP: ['Barış ve Demokrasi Partisi'],
+    CGP: ['Cumhuriyetçi Güven Partisi', 'Güven Partisi'],
+    CHP: ['Cumhuriyet Halk Partisi'],
+    CKMP: ['Cumhuriyetçi Köylü Millet Partisi'],
+    CMP: ['Cumhuriyetçi Millet Partisi'],
+    DBP: ['Demokratik Bölgeler Partisi'],
+    DP46: ['Demokrat Parti'],
+    DP70: ['Demokratik Parti'],
+    DSP: ['Demokratik Sol Parti'],
+    DTP: ['Demokratik Türkiye Partisi'],
+    DYP: ['Doğru Yol Partisi'],
+    FP:  ['Fazilet Partisi'],
+    HDP: ['Halkların Demokratik Partisi'],
+    HP:  ['Halkçı Parti'],
+    HP55:  ['Hürriyet Partisi'],
+    KADEP: ['Katılımcı Demokrasi Partisi'],
+    MDP: ['Milliyetçi Demokrasi Partisi'],
+    MHP: ['Milliyetçi Hareket Partisi'],
+    MÇP: ['Milliyetçi Çalışma Partisi'],
+    MP:  ['Millet Partisi'],
+    MSP: ['Millî Selamet Partisi', 'Milli Selamet Partisi'],
+    RP:  ['Refah Partisi'],
+    SHP: ['Sosyaldemokrat Halkçı Parti', 'Sosyal Demokrat Halkçı Parti'],
+    TBP: ['Türkiye Birlik Partisi', 'Birlik Partisi'],
+    TİP: ['Türkiye İşçi Partisi'],
+    YTP: ['Yeni Türkiye Partisi'],
+    ind: ['Bağımsız']
+  }.freeze
+
+  def initialize(party_name)
+    @party_name = party_name.to_s
+  end
+
+  def id
+    @id ||= match && match.first.to_s
+  end
+
+  def name
+    @name ||= match && match.last.first
+  end
+
+  private
+
+  attr_reader :party_name
+
+  def match
+    @match ||= MAPPING.find { |_id, ns| ns.include? party }
+  end
+
+  def party
+    @party ||=
+      begin
+        if party_name.include? '→'
+          party_name.split(/\s*→\s*/).first
+        else
+          party_name
+        end
+      end
+  end
+end
--- scraper.rb
+++ scraper.rb
@@ -32,60 +32,16 @@ terms = {
   TermSinglePartyPage => [ 7, 6, 5, 4, 3, 2, 1 ],
 }
 
-PARTY = {
-  AKP: ['Adalet ve Kalkınma Partisi'],
-  ANAP: ['Anavatan Partisi'],
-  AP:  ['Adalet Partisi'],
-  BBP: ['Büyük Birlik Partisi'],
-  BDP: ['Barış ve Demokrasi Partisi'],
-  CGP: ['Cumhuriyetçi Güven Partisi', 'Güven Partisi'],
-  CHP: ['Cumhuriyet Halk Partisi'],
-  CKMP: ['Cumhuriyetçi Köylü Millet Partisi'],
-  CMP: ['Cumhuriyetçi Millet Partisi'],
-  DBP: ['Demokratik Bölgeler Partisi'],
-  DP46: ['Demokrat Parti'],
-  DP70: ['Demokratik Parti'],
-  DSP: ['Demokratik Sol Parti'],
-  DTP: ['Demokratik Türkiye Partisi'],
-  DYP: ['Doğru Yol Partisi'],
-  FP:  ['Fazilet Partisi'],
-  HDP: ['Halkların Demokratik Partisi'],
-  HP:  ['Halkçı Parti'],
-  HP55:  ['Hürriyet Partisi'],
-  KADEP: ['Katılımcı Demokrasi Partisi'],
-  MDP: ['Milliyetçi Demokrasi Partisi'],
-  MHP: ['Milliyetçi Hareket Partisi'],
-  MÇP: ['Milliyetçi Çalışma Partisi'],
-  MP:  ['Millet Partisi'],
-  MSP: ['Millî Selamet Partisi', 'Milli Selamet Partisi'],
-  RP:  ['Refah Partisi'],
-  SHP: ['Sosyaldemokrat Halkçı Parti', 'Sosyal Demokrat Halkçı Parti'],
-  TBP: ['Türkiye Birlik Partisi', 'Birlik Partisi'],
-  TİP: ['Türkiye İşçi Partisi'],
-  YTP: ['Yeni Türkiye Partisi'],
-  ind: ['Bağımsız'],
-}
-
-def party_from(party)
-  party = party.split(/\s*→\s*/).first if party.include? '→'
-  found = PARTY.find { |id, ns| ns.include? party } or binding.pry
-  {
-    party_id: found.first.to_s,
-    party: found.last.first,
-  }
-end
-
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
 terms.each do |klass, ts|
   ts.each do |t|
     url = "https://tr.wikipedia.org/wiki/TBMM_#{t}._d%C3%B6nem_milletvekilleri_listesi"
     response = Scraped::Request.new(url: url).response
     data = klass.new(response: response).members.map { |m| 
-      binding.pry if m[:party].to_s.empty?
-      m.merge(party_from(m[:party])).merge(term: t, source: url, id: id_for(m))
+      party_info = PartyInformation.new(m[:party])
+      m.merge(party: party_info.name, party_id: party_info.id, term: t, source: url, id: id_for(m))
     }
     warn "#{t}: #{data.count}"
-    data.find_all { |m| m[:party][/[0-9]/] }.each { |m| puts m.to_s.magenta }
     ScraperWiki.save_sqlite([:id, :area, :term], data)
   end
 end
