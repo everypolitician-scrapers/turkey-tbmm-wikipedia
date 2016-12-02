acee9a04edff44ee27fc0be93c48a87c40570b35
Chris Mytton <chrismytton@gmail.com>
2016-12-02 14:59:26 +0100

    Add methods to TermTableRow::Base for other fields
    These are the fields that are common between all rows.

--- lib/term_table_row/base.rb
+++ lib/term_table_row/base.rb
@@ -3,5 +3,27 @@ module TermTableRow
     def self.expected_headers(expected = nil)
       @expected_headers ||= expected
     end
+
+    field :id do
+      [wikipedia__tr, name].find { |n| !n.to_s.empty? }.downcase.gsub(/[[:space:]]/,'_')
+    end
+
+    field :source do
+      url.to_s
+    end
+
+    field :party_id do
+      party_info.id
+    end
+
+    field :party do
+      party_info.name
+    end
+
+    private
+
+    def party_info
+      @party_info = PartyInformation.new(party_name)
+    end
   end
 end
