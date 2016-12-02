48de0f0aa2071f46d86f7dcc0753a149b78a7302
Chris Mytton <chrismytton@gmail.com>
2016-12-02 15:23:48 +0100

    Remove unused RemoveUnwantedTable class
    Now that we're auto-detecting the correct class for given tables we no
    longer need this as we just ignore tables that we can't process.

--- lib/remove_unwanted_table.rb
+++
@@ -1,7 +0,0 @@
-class RemoveUnwantedTable < Scraped::Response::Decorator
-  def body
-    doc = Nokogiri::HTML(super)
-    doc.xpath(".//table[.//th[4][contains(.,'Değişim')]]").remove
-    doc.to_s
-  end
-end
