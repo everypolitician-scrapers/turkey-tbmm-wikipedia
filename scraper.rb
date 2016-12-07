789c6812e520c68deb563444be61efb23f9b01a7
Chris Mytton <chrismytton@gmail.com>
2016-12-07 19:15:24 +0100

    Use unstable urls for terms that have been edited
    I've had to fix the markup in a few places, so link to the unstable
    versions of those pages until the changes are approved.

--- scraper.rb
+++ scraper.rb
@@ -18,6 +18,9 @@ ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
 
 26.downto(1) do |t|
   url = "https://tr.wikipedia.org/wiki/TBMM_#{t}._d%C3%B6nem_milletvekilleri_listesi"
+  url = 'https://tr.wikipedia.org/w/index.php?title=TBMM_18._d%C3%B6nem_milletvekilleri_listesi&stable=0' if t == 18
+  url = 'https://tr.wikipedia.org/w/index.php?title=TBMM_16._d%C3%B6nem_milletvekilleri_listesi&stable=0' if t == 16
+  url = 'https://tr.wikipedia.org/w/index.php?title=TBMM_9._d%C3%B6nem_milletvekilleri_listesi&stable=0'  if t == 9
   response = Scraped::Request.new(url: url).response
   data = TermPage.new(response: response).members
   warn "#{t}: #{data.count}"
