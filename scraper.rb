214ed75ca538c774b626fdb5ebecd384665b478c
Tony Bowden <tony@tmtm.com>
2017-09-11 12:22:34 +0100

    Show debug output if MORPH_DEBUG is set

--- scraper.rb
+++ scraper.rb
@@ -22,7 +22,7 @@ ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
   url = "https://tr.wikipedia.org/wiki/TBMM_#{t}._d%C3%B6nem_milletvekilleri_listesi"
   url = 'https://tr.wikipedia.org/w/index.php?title=TBMM_21._d%C3%B6nem_milletvekilleri_listesi&stable=0' if t == 21
   response = Scraped::Request.new(url: url).response
-  data = TermPage.new(response: response).members
-  warn "#{t}: #{data.count}"
-  ScraperWiki.save_sqlite(%i[id area term], data.map { |m| m.to_h.merge(term: t) })
+  data = TermPage.new(response: response).members.map { |m| m.to_h.merge(term: t) }
+  data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
+  ScraperWiki.save_sqlite(%i[id area term], data)
 end
