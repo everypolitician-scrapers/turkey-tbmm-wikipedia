d5b07ad81265a5e34b024894e69718b9b828ad61
Tony Bowden <tony@tmtm.com>
2017-09-12 06:26:51 +0100

    Use unstable version of Term 16 for now
    There was a bug in the cell spanning that was confusing things. I've
    fixed it at source, but until that's approved, use the unstable version
    of the page.

--- scraper.rb
+++ scraper.rb
@@ -14,7 +14,7 @@ ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
 
 26.downto(1) do |t|
   url = "https://tr.wikipedia.org/wiki/TBMM_#{t}._d%C3%B6nem_milletvekilleri_listesi"
-  url = 'https://tr.wikipedia.org/w/index.php?title=TBMM_21._d%C3%B6nem_milletvekilleri_listesi&stable=0' if t == 21
+  url = 'https://tr.wikipedia.org/w/index.php?title=TBMM_16._d%C3%B6nem_milletvekilleri_listesi&stable=0' if t == 16
   response = Scraped::Request.new(url: url).response
   data = TermPage.new(response: response).members.map { |m| m.to_h.merge(term: t) }
   data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
